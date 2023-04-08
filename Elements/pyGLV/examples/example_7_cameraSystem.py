32c1c883ae0f663dfbe5dec024ffaacab725c262
Antonis Protopsaltis <aprotopsaltis@oramavr.com>
2023-04-08 18:23:16 +0300

    Change mouse interaction to a blender-like fashion to stop conflicting with imgui (left click)

--- Elements/pyGLV/examples/example_7_cameraSystem.py
+++ Elements/pyGLV/examples/example_7_cameraSystem.py
@@ -13,10 +13,10 @@ from __future__         import annotations
 from asyncore import dispatcher
 from math import sin, cos, radians
 from enum import Enum
-from random import uniform
+from random import uniform;
 import numpy as np
 
-import OpenGL.GL as gl
+import OpenGL.GL as gl;
 import Elements.pyECSS.utilities as util
 from Elements.pyECSS.System import  TransformSystem, CameraSystem
 from Elements.pyECSS.Entity import Entity
@@ -28,52 +28,87 @@ from Elements.pyGLV.GL.Shader import InitGLShaderSystem, Shader, ShaderGLDecorat
 from Elements.pyGLV.GL.VertexArray import VertexArray
 from Elements.pyGLV.GL.Scene import Scene
 from Elements.pyGLV.GL.SimpleCamera import SimpleCamera
-from Elements.pyGLV.utils.normals import Convert
-from OpenGL.GL import GL_LINES
 
+
+class IndexedConverter():
+    
+    # Assumes triangulated buffers. Produces indexed results that support
+    # normals as well.
+    def Convert(self, vertices, colors, indices, produceNormals=True):
+
+        iVertices = [];
+        iColors = [];
+        iNormals = [];
+        iIndices = [];
+        for i in range(0, len(indices), 3):
+            iVertices.append(vertices[indices[i]]);
+            iVertices.append(vertices[indices[i + 1]]);
+            iVertices.append(vertices[indices[i + 2]]);
+            iColors.append(colors[indices[i]]);
+            iColors.append(colors[indices[i + 1]]);
+            iColors.append(colors[indices[i + 2]]);
+            
+
+            iIndices.append(i);
+            iIndices.append(i + 1);
+            iIndices.append(i + 2);
+
+        if produceNormals:
+            for i in range(0, len(indices), 3):
+                iNormals.append(util.calculateNormals(vertices[indices[i]], vertices[indices[i + 1]], vertices[indices[i + 2]]));
+                iNormals.append(util.calculateNormals(vertices[indices[i]], vertices[indices[i + 1]], vertices[indices[i + 2]]));
+                iNormals.append(util.calculateNormals(vertices[indices[i]], vertices[indices[i + 1]], vertices[indices[i + 2]]));
+
+        iVertices = np.array( iVertices, dtype=np.float32 )
+        iColors   = np.array( iColors,   dtype=np.float32 )
+        iIndices  = np.array( iIndices,  dtype=np.uint32  )
+
+        iNormals  = np.array( iNormals,  dtype=np.float32 )
+
+        return iVertices, iColors, iIndices, iNormals;
 
 class GameObjectEntity(Entity):
     def __init__(self, name=None, type=None, id=None) -> None:
-        super().__init__(name, type, id)
+        super().__init__(name, type, id);
 
         # Gameobject basic properties
-        self._color          = [1, 0.5, 0.2, 1.0] # this will be used as a uniform var
+        self._color          = [1, 0.5, 0.2, 1.0]; # this will be used as a uniform var
         # Create basic components of a primitive object
-        self.trans          = BasicTransform(name="trans", trs=util.identity())
-        self.mesh           = RenderMesh(name="mesh")
-        # self.shaderDec      = ShaderGLDecorator(Shader(vertex_source=Shader.VERT_PHONG_MVP, fragment_source=Shader.FRAG_PHONG))
-        self.shaderDec      = ShaderGLDecorator(Shader(vertex_source= Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG))
-        self.vArray         = VertexArray()
+        self.trans          = BasicTransform(name="trans", trs=util.identity());
+        self.mesh           = RenderMesh(name="mesh");
+        # self.shaderDec      = ShaderGLDecorator(Shader(vertex_source=Shader.VERT_PHONG_MVP, fragment_source=Shader.FRAG_PHONG));
+        self.shaderDec      = ShaderGLDecorator(Shader(vertex_source= Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG));
+        self.vArray         = VertexArray();
         # Add components to entity
-        scene = Scene()
-        scene.world.createEntity(self)
-        scene.world.addComponent(self, self.trans)
-        scene.world.addComponent(self, self.mesh)
-        scene.world.addComponent(self, self.shaderDec)
-        scene.world.addComponent(self, self.vArray)
+        scene = Scene();
+        scene.world.createEntity(self);
+        scene.world.addComponent(self, self.trans);
+        scene.world.addComponent(self, self.mesh);
+        scene.world.addComponent(self, self.shaderDec);
+        scene.world.addComponent(self, self.vArray);
 
     @property
     def color(self):
-        return self._color
+        return self._color;
     @color.setter
     def color(self, colorArray):
-        self._color = colorArray
+        self._color = colorArray;
 
     def drawSelfGui(self, imgui):
-        changed, value = imgui.color_edit3("Color", self.color[0], self.color[1], self.color[2])
-        self.color = [value[0], value[1], value[2], 1.0]
+        changed, value = imgui.color_edit3("Color", self.color[0], self.color[1], self.color[2]);
+        self.color = [value[0], value[1], value[2], 1.0];
 
     def SetVertexAttributes(self, vertex, color, index, normals = None):
-        self.mesh.vertex_attributes.append(vertex)
-        self.mesh.vertex_attributes.append(color)
+        self.mesh.vertex_attributes.append(vertex);
+        self.mesh.vertex_attributes.append(color);
         if normals is not None:
-            self.mesh.vertex_attributes.append(normals)
-        self.mesh.vertex_index.append(index)
+            self.mesh.vertex_attributes.append(normals);
+        self.mesh.vertex_index.append(index);
 
 
 
 def CubeSpawn(cubename = "Cube"): 
-    cube = GameObjectEntity(cubename)
+    cube = GameObjectEntity(cubename);
     vertices = [
         [-0.5, -0.5, 0.5, 1.0],
         [-0.5, 0.5, 0.5, 1.0],
@@ -83,7 +118,7 @@ def CubeSpawn(cubename = "Cube"):
         [-0.5, 0.5, -0.5, 1.0], 
         [0.5, 0.5, -0.5, 1.0], 
         [0.5, -0.5, -0.5, 1.0]
-    ]
+    ];
     colors = [
         [1.0, 0.0, 0.0, 1.0],
         [1.0, 0.5, 0.0, 1.0],
@@ -93,7 +128,7 @@ def CubeSpawn(cubename = "Cube"):
         [0.0, 1.0, 1.0, 1.0],
         [0.0, 1.0, 0.0, 1.0],
         [0.0, 1.0, 0.0, 1.0]                    
-    ]
+    ];
     # OR
     # colors =  [cube.color] * len(vertices) 
     
@@ -111,10 +146,10 @@ def CubeSpawn(cubename = "Cube"):
         dtype=np.uint32
     ) #rhombus out of two triangles
 
-    vertices, colors, indices, normals = Convert(vertices, colors, indices, produceNormals=True)
-    cube.SetVertexAttributes(vertices, colors, indices, normals)
+    vertices, colors, indices, normals = IndexedConverter().Convert(vertices, colors, indices, produceNormals=True);
+    cube.SetVertexAttributes(vertices, colors, indices, normals);
     
-    return cube
+    return cube;
 
 
 def main(imguiFlag = False):
@@ -135,38 +170,24 @@ def main(imguiFlag = False):
     rootEntity = scene.world.createEntity(Entity(name="Root"))
 
     # Spawn Camera
-    mainCamera = SimpleCamera("Simple Camera")
+    mainCamera = SimpleCamera("Simple Camera");
     # Camera Settings
     mainCamera.trans2.trs = util.translate(0, 0, 8) # VIEW
-    mainCamera.trans1.trs = util.rotate((1, 0, 0), -45) 
+    mainCamera.trans1.trs = util.rotate((1, 0, 0), -45); 
 
     # Spawn Two Homes on top of each other
-    home1 = scene.world.createEntity(Entity("Home"))
-    scene.world.addEntityChild(scene.world.root, home1)
-    trans = BasicTransform(name="trans", trs=util.identity())    
-    scene.world.addComponent(home1, trans)
+    home1 = scene.world.createEntity(Entity("Home"));
+    scene.world.addEntityChild(scene.world.root, home1);
+    trans = BasicTransform(name="trans", trs=util.identity());    
+    scene.world.addComponent(home1, trans);
     cube_bot: GameObjectEntity = CubeSpawn("BOT CUBE")
-    scene.world.addEntityChild(home1, cube_bot)
+    scene.world.addEntityChild(home1, cube_bot);
     cube_top: GameObjectEntity = CubeSpawn()
-    scene.world.addEntityChild(home1, cube_top)
-    home1.getChild(0).trs = util.translate(0, 0, 0)
-    cube_top.trans.trs = util.translate(0, 1, 0)
+    scene.world.addEntityChild(home1, cube_top);
+    home1.getChild(0).trs = util.translate(0, 0, 0);
+    cube_top.trans.trs = util.translate(0, 1, 0);
     cube_top.name = "TOP CUBE"
     
-    # Generate terrain
-    from Elements.pyGLV.utils.terrain import generateTerrain
-    vertexTerrain, indexTerrain, colorTerrain= generateTerrain(size=4,N=20)
-    # Add terrain
-    terrain = scene.world.createEntity(Entity(name="terrain"))
-    scene.world.addEntityChild(rootEntity, terrain)
-    terrain_trans = scene.world.addComponent(terrain, BasicTransform(name="terrain_trans", trs=util.identity()))
-    terrain_mesh = scene.world.addComponent(terrain, RenderMesh(name="terrain_mesh"))
-    terrain_mesh.vertex_attributes.append(vertexTerrain) 
-    terrain_mesh.vertex_attributes.append(colorTerrain)
-    terrain_mesh.vertex_index.append(indexTerrain)
-    terrain_vArray = scene.world.addComponent(terrain, VertexArray(primitive=GL_LINES))
-    terrain_shader = scene.world.addComponent(terrain, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
-    # terrain_shader.setUniformVariable(key='modelViewProj', value=mvpMat, mat4=True)
     
     # MAIN RENDERING LOOP
     running = True
@@ -182,11 +203,11 @@ def main(imguiFlag = False):
     # ---------------------------------------------------------
     
     gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-    gl.glDisable(gl.GL_CULL_FACE)
+    gl.glDisable(gl.GL_CULL_FACE);
 
-    # gl.glDepthMask(gl.GL_FALSE)  
-    gl.glEnable(gl.GL_DEPTH_TEST)
-    gl.glDepthFunc(gl.GL_LESS)
+    # gl.glDepthMask(gl.GL_FALSE);  
+    gl.glEnable(gl.GL_DEPTH_TEST);
+    gl.glDepthFunc(gl.GL_LESS);
     scene.world.traverse_visit(initUpdate, scene.world.root)
     
 
@@ -231,10 +252,10 @@ def main(imguiFlag = False):
         scene.world.traverse_visit(transUpdate, scene.world.root) 
         scene.world.traverse_visit_pre_camera(camUpdate, mainCamera.camera)
         scene.world.traverse_visit(camUpdate, scene.world.root)
-        home1.getChild(1).shaderDec.setUniformVariable(key='modelViewProj', value=home1.getChild(1).trans.l2cam, mat4=True)
-        home1.getChild(2).shaderDec.setUniformVariable(key='modelViewProj', value=home1.getChild(2).trans.l2cam, mat4=True)
+        home1.getChild(1).shaderDec.setUniformVariable(key='modelViewProj', value=home1.getChild(1).trans.l2cam, mat4=True);
+        home1.getChild(2).shaderDec.setUniformVariable(key='modelViewProj', value=home1.getChild(2).trans.l2cam, mat4=True);
                 
-        home1.getChild(1).shaderDec.setUniformVariable(key='my_color', value=[0.4, 0.4, 0.4, 1.0], float4=True)
+        home1.getChild(1).shaderDec.setUniformVariable(key='my_color;', value=[0.4, 0.4, 0.4, 1.0], float4=True);
         # call SDLWindow/ImGUI display() and ImGUI event input process
         running = scene.render()
         # call the GL State render System
