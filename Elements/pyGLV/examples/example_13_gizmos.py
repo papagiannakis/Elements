9bc4eaa4fc3c778a8ba9286631209a01175f59f9
Chibi Nemesis <paulzotas@hotmail.com>
2023-04-27 21:22:13 +0300

    fix some issues regarding selecting elements and add unit test for empty scene with gizmos

---
+++ Elements/pyGLV/examples/example_13_gizmos.py
@@ -0,0 +1,187 @@
+import numpy as np
+
+import Elements.pyECSS.utilities as util
+from Elements.pyECSS.Entity import Entity
+from Elements.pyECSS.Component import BasicTransform, RenderMesh
+from Elements.pyECSS.System import  TransformSystem
+from Elements.pyGLV.GL.Scene import Scene
+from Elements.pyGLV.GUI.Viewer import RenderGLStateSystem
+from Elements.pyGLV.Gizmos.Gizmos import Gizmos
+
+from Elements.pyGLV.GL.Shader import InitGLShaderSystem, Shader, ShaderGLDecorator, RenderGLShaderSystem
+from Elements.pyGLV.GL.VertexArray import VertexArray
+
+from OpenGL.GL import GL_LINES
+
+scene = Scene()    
+
+# Scenegraph with Entities, Components
+rootEntity = scene.world.createEntity(Entity(name="RooT"))
+entityCam1 = scene.world.createEntity(Entity(name="entityCam1"))
+scene.world.addEntityChild(rootEntity, entityCam1)
+trans1 = scene.world.addComponent(entityCam1, BasicTransform(name="trans1", trs=util.translate(2.5, 2.5, 2.5)))
+
+node4 = scene.world.createEntity(Entity(name="node4"))
+scene.world.addEntityChild(rootEntity, node4)
+trans4 = scene.world.addComponent(node4, BasicTransform(name="trans4", trs=util.translate(0.0,0.5,-1.5))) #util.identity()
+mesh4 = scene.world.addComponent(node4, RenderMesh(name="mesh4"))
+
+node4_2 = scene.world.createEntity(Entity(name="node4_2"))
+scene.world.addEntityChild(rootEntity, node4_2)
+trans4_2 = scene.world.addComponent(node4_2, BasicTransform(name="trans4_2", trs=util.translate(0.0,0.5,0.0))) #util.identity()
+mesh4_2 = scene.world.addComponent(node4_2, RenderMesh(name="mesh4_2"))
+
+
+# a simple triangle
+vertexData = np.array([
+    [0.0, 0.0, 0.0, 1.0],
+    [0.5, 1.0, 0.0, 1.0],
+    [1.0, 0.0, 0.0, 1.0]
+],dtype=np.float32) 
+colorVertexData = np.array([
+    [1.0, 0.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 1.0]
+], dtype=np.float32)
+
+#Simple Cube
+vertexCube = np.array([
+    [-0.5, -0.5, 0.5, 1.0],
+    [-0.5, 0.5, 0.5, 1.0],
+    [0.5, 0.5, 0.5, 1.0],
+    [0.5, -0.5, 0.5, 1.0], 
+    [-0.5, -0.5, -0.5, 1.0], 
+    [-0.5, 0.5, -0.5, 1.0], 
+    [0.5, 0.5, -0.5, 1.0], 
+    [0.5, -0.5, -0.5, 1.0]
+],dtype=np.float32) 
+colorCube = np.array([
+    [0.0, 0.0, 1.0, 1.0],
+    [0.0, 0.0, 1.0, 1.0],
+    [0.0, 0.0, 1.0, 1.0],
+    [0.0, 0.0, 1.0, 1.0],
+    [0.0, 0.0, 1.0, 1.0],
+    [0.0, 0.0, 1.0, 1.0],
+    [0.0, 0.0, 1.0, 1.0],
+    [0.0, 0.0, 1.0, 1.0]
+], dtype=np.float32)
+colorCube2 = np.array([
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 1.0, 0.0, 1.0]
+], dtype=np.float32)
+
+#index arrays for above vertex Arrays
+index = np.array((0,1,2), np.uint32) #simple triangle
+indexCube = np.array((1,0,3, 1,3,2, 
+                  2,3,7, 2,7,6,
+                  3,0,4, 3,4,7,
+                  6,5,1, 6,1,2,
+                  4,5,6, 4,6,7,
+                  5,4,0, 5,0,1), np.uint32) #rhombus out of two triangles
+
+
+
+# Systems
+transUpdate = scene.world.createSystem(TransformSystem("transUpdate", "TransformSystem", "001"))
+# camUpdate = scene.world.createSystem(CameraSystem("camUpdate", "CameraUpdate", "200"))
+renderUpdate = scene.world.createSystem(RenderGLShaderSystem())
+initUpdate = scene.world.createSystem(InitGLShaderSystem())
+
+
+## ADD CUBE ##
+# attach a simple cube in a RenderMesh so that VertexArray can pick it up
+mesh4.vertex_attributes.append(vertexCube)
+mesh4.vertex_attributes.append(colorCube)
+mesh4.vertex_index.append(indexCube)
+vArray4 = scene.world.addComponent(node4, VertexArray())
+shaderDec4 = scene.world.addComponent(node4, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+
+mesh4_2.vertex_attributes.append(vertexCube)
+mesh4_2.vertex_attributes.append(colorCube2)
+mesh4_2.vertex_index.append(indexCube)
+vArray4_2 = scene.world.addComponent(node4_2, VertexArray())
+shaderDec4_2 = scene.world.addComponent(node4_2, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+
+# Generate terrain
+from Elements.pyGLV.utils.terrain import generateTerrain
+vertexTerrain, indexTerrain, colorTerrain= generateTerrain(size=4,N=20)
+# Add terrain
+terrain = scene.world.createEntity(Entity(name="terrain"))
+scene.world.addEntityChild(rootEntity, terrain)
+terrain_trans = scene.world.addComponent(terrain, BasicTransform(name="terrain_trans", trs=util.identity()))
+terrain_mesh = scene.world.addComponent(terrain, RenderMesh(name="terrain_mesh"))
+terrain_mesh.vertex_attributes.append(vertexTerrain) 
+terrain_mesh.vertex_attributes.append(colorTerrain)
+terrain_mesh.vertex_index.append(indexTerrain)
+terrain_vArray = scene.world.addComponent(terrain, VertexArray(primitive=GL_LINES))
+terrain_shader = scene.world.addComponent(terrain, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+
+eye = util.vec(2.5, 2.5, 2.5)
+target = util.vec(0.0, 0.0, 0.0)
+up = util.vec(0.0, 1.0, 0.0)
+view = util.lookat(eye, target, up)
+fov = 50.0
+aspect_ratio = 1.0
+near = 0.01
+far = 10.0
+projMat = util.perspective(fov, aspect_ratio, near, far) 
+
+gizmos = Gizmos(rootEntity,projMat,view)
+gizmos.set_camera_in_use("entityCam1")
+
+# MAIN RENDERING LOOP
+
+running = True
+scene.init(imgui=True, windowWidth = 1024, windowHeight = 768, windowTitle = "Elements: An Example with Gizmos", openGLversion = 4)
+
+# pre-pass scenegraph to initialise all GL context dependent geometry, shader classes
+# needs an active GL context
+scene.world.traverse_visit(initUpdate, scene.world.root)    
+
+################### EVENT MANAGER ###################
+
+eManager = scene.world.eventManager
+gWindow = scene.renderWindow
+gGUI = scene.gContext
+
+renderGLEventActuator = RenderGLStateSystem()
+
+
+eManager._subscribers['OnUpdateWireframe'] = gWindow
+eManager._actuators['OnUpdateWireframe'] = renderGLEventActuator
+eManager._subscribers['OnUpdateCamera'] = gWindow 
+eManager._actuators['OnUpdateCamera'] = renderGLEventActuator
+
+gWindow._myCamera = view # otherwise, an imgui slider must be moved to properly update
+
+
+model_cube = trans4.trs
+
+model_terrain = terrain.getChild(0).trs # notice that terrain.getChild(0) == terrain_trans
+
+model_cube2 = trans4_2.trs
+
+while running:
+    running = scene.render(running)
+    scene.world.traverse_visit(renderUpdate, scene.world.root)
+    view =  gWindow._myCamera # updates view via the imgui
+    gizmos.update_mouse_position()
+    gizmos.update_view(view)
+    gizmos.get_keyboard_Event()
+    gizmos.update_gizmos()
+    mvp_cube = projMat @ view @ model_cube
+    mvp_cube2 = projMat @ view @ model_cube2
+    mvp_terrain = projMat @ view @ model_terrain
+
+    terrain_shader.setUniformVariable(key='modelViewProj', value=mvp_terrain, mat4=True)
+    shaderDec4.setUniformVariable(key='modelViewProj', value=mvp_cube, mat4=True)
+    shaderDec4_2.setUniformVariable(key='modelViewProj', value=mvp_cube2, mat4=True)
+    scene.render_post()
+    
+scene.shutdown()
