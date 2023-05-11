5799ec70de6efbd7c41d796062e8c930b5b89302
Chibi Nemesis <paulzotas@hotmail.com>
2023-05-11 23:39:38 +0300

    Add 2 more unit tests

--- Elements/pyGLV/tests/test_gizmos.py
+++ Elements/pyGLV/tests/test_gizmos.py
@@ -38,12 +38,52 @@ class TestGizmos(unittest.TestCase):
         self.camUpdate = self.scene.world.createSystem(CameraSystem("camUpdate", "CameraUpdate", "200"))
         self.renderUpdate = self.scene.world.createSystem(RenderGLShaderSystem())
         self.initUpdate = self.scene.world.createSystem(InitGLShaderSystem())
+
+        self.vertexCube = np.array([
+            [-0.5, -0.5, 0.5, 1.0],
+            [-0.5, 0.5, 0.5, 1.0],
+            [0.5, 0.5, 0.5, 1.0],
+            [0.5, -0.5, 0.5, 1.0], 
+            [-0.5, -0.5, -0.5, 1.0], 
+            [-0.5, 0.5, -0.5, 1.0], 
+            [0.5, 0.5, -0.5, 1.0], 
+            [0.5, -0.5, -0.5, 1.0]
+        ],dtype=np.float32)
+        self.colorCube = np.array([
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0]
+        ], dtype=np.float32)
+        self.colorCube2 = np.array([
+            [0.0, 1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0, 1.0]
+        ], dtype=np.float32)
+
+        self.indexCube = np.array((1,0,3, 1,3,2, 
+                  2,3,7, 2,7,6,
+                  3,0,4, 3,4,7,
+                  6,5,1, 6,1,2,
+                  4,5,6, 4,6,7,
+                  5,4,0, 5,0,1), np.uint32) 
+        
+
     
     def testEmpty(self):
         """
         Test Gizmos on an empty scene
         """
-        eye = util.vec(0.5, 0.5, 0.5)
+        eye = util.vec(2.5, 2.5, 2.5)
         target = util.vec(0,0,0)
         up = util.vec(0.0, 1.0, 0.0)
         view = util.lookat(eye, target, up)
@@ -56,6 +96,8 @@ class TestGizmos(unittest.TestCase):
 
         gizmos = Gizmos(self.rootEntity,projMat,view)
         gizmos.set_camera_in_use("entityCam1")
+        gizmos.update_projection(projMat)
+        gizmos.update_projection_args(window_width=1024,window_height=768,fov=fov)
 
         running = True
         # MAIN RENDERING LOOP
@@ -67,6 +109,7 @@ class TestGizmos(unittest.TestCase):
             running = self.scene.render(running)
             self.scene.world.traverse_visit(self.renderUpdate, self.scene.world.root)
             gizmos.update_mouse_position()
+            gizmos.update_view(view)
             gizmos.get_keyboard_Event()
             gizmos.update_gizmos()
             gizmos.update_imgui()
@@ -78,10 +121,151 @@ class TestGizmos(unittest.TestCase):
         """
         Test gizmos on a scene with a single element
         """
-        pass
+        node4 = self.scene.world.createEntity(Entity(name="node4"))
+        self.scene.world.addEntityChild(self.rootEntity, node4)
+        trans4 = self.scene.world.addComponent(node4, BasicTransform(name="trans4", trs=util.translate(0.0,0.5,-0.5)))
+        mesh4 = self.scene.world.addComponent(node4, RenderMesh(name="mesh4"))
+
+        mesh4.vertex_attributes.append(self.vertexCube)
+        mesh4.vertex_attributes.append(self.colorCube)
+        mesh4.vertex_index.append(self.indexCube)
+        vArray4 = self.scene.world.addComponent(node4, VertexArray())
+        shaderDec4 = self.scene.world.addComponent(node4, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+        
+        eye = util.vec(2.5, 2.5, 2.5)
+        target = util.vec(0,0,0)
+        up = util.vec(0.0, 1.0, 0.0)
+        view = util.lookat(eye, target, up)
+
+        fov = 50.0
+        aspect_ratio = 1.0
+        near = 0.01
+        far = 10.0
+        projMat = util.perspective(fov, aspect_ratio, near, far) 
+
+        gizmos = Gizmos(self.rootEntity,projMat,view)
+        gizmos.set_camera_in_use("entityCam1")
+        gizmos.update_projection(projMat)
+        gizmos.update_projection_args(window_width=1024,window_height=768,fov=fov)
+
+        running = True
+        self.scene.init(imgui=True, windowWidth = 1024, windowHeight = 768, windowTitle = "Elements test_gizmos_Single_Element")
+
+        self.scene.world.traverse_visit(self.initUpdate,self.scene.world.root)
+
+        eManager = self.scene.world.eventManager
+        gWindow = self.scene.renderWindow
+        gGUI = self.scene.gContext
+
+        renderGLEventActuator = RenderGLStateSystem()
+
+
+        eManager._subscribers['OnUpdateWireframe'] = gWindow
+        eManager._actuators['OnUpdateWireframe'] = renderGLEventActuator
+        eManager._subscribers['OnUpdateCamera'] = gWindow 
+        eManager._actuators['OnUpdateCamera'] = renderGLEventActuator
+
+        gWindow._myCamera = view
+
+        while running:
+            running = self.scene.render(running)
+            self.scene.world.traverse_visit(self.renderUpdate, self.scene.world.root)
+            view =  gWindow._myCamera
+            gizmos.update_mouse_position()
+            gizmos.update_view(view)
+            gizmos.get_keyboard_Event()
+            gizmos.update_gizmos()
+            gizmos.update_imgui()
+            model_cube = trans4.trs
+            mvp_cube = projMat @ view @ model_cube
+
+            shaderDec4.setUniformVariable(key='modelViewProj', value=mvp_cube, mat4=True)
+
+            self.scene.render_post()
+            
+        self.scene.shutdown()
+
 
     def testMultiple(self):
         """
-        Test gizmos on a scene with multiple elements
+        Test gizmos on a scene with multiple elements, where one element is child of another
         """
-        pass
+        node4 = self.scene.world.createEntity(Entity(name="node4"))
+        self.scene.world.addEntityChild(self.rootEntity, node4)
+        trans4 = self.scene.world.addComponent(node4, BasicTransform(name="trans4", trs=util.translate(0.0,0.5,-1.5)))
+        mesh4 = self.scene.world.addComponent(node4, RenderMesh(name="mesh4"))
+
+        node4_2 = self.scene.world.createEntity(Entity(name="node4_2"))
+        self.scene.world.addEntityChild(node4, node4_2)
+        trans4_2 = self.scene.world.addComponent(node4_2, BasicTransform(name="trans4_2", trs=util.translate(0.0,0.5,0.0)))
+        mesh4_2 = self.scene.world.addComponent(node4_2, RenderMesh(name="mesh4_2"))
+
+        mesh4.vertex_attributes.append(self.vertexCube)
+        mesh4.vertex_attributes.append(self.colorCube)
+        mesh4.vertex_index.append(self.indexCube)
+        vArray4 = self.scene.world.addComponent(node4, VertexArray())
+        shaderDec4 = self.scene.world.addComponent(node4, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+
+        mesh4_2.vertex_attributes.append(self.vertexCube)
+        mesh4_2.vertex_attributes.append(self.colorCube2)
+        mesh4_2.vertex_index.append(self.indexCube)
+        vArray4_2 = self.scene.world.addComponent(node4_2, VertexArray())
+        shaderDec4_2 = self.scene.world.addComponent(node4_2, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+
+
+        eye = util.vec(2.5, 2.5, 2.5)
+        target = util.vec(0,0,0)
+        up = util.vec(0.0, 1.0, 0.0)
+        view = util.lookat(eye, target, up)
+
+        fov = 50.0
+        aspect_ratio = 1.0
+        near = 0.01
+        far = 10.0
+        projMat = util.perspective(fov, aspect_ratio, near, far) 
+
+        gizmos = Gizmos(self.rootEntity,projMat,view)
+        gizmos.set_camera_in_use("entityCam1")
+        gizmos.update_projection(projMat)
+        gizmos.update_projection_args(window_width=1024,window_height=768,fov=fov)
+
+        running = True
+        self.scene.init(imgui=True, windowWidth = 1024, windowHeight = 768, windowTitle = "Elements test_gizmos_Multiple_Elements")
+
+        self.scene.world.traverse_visit(self.initUpdate,self.scene.world.root)
+
+        eManager = self.scene.world.eventManager
+        gWindow = self.scene.renderWindow
+        gGUI = self.scene.gContext
+
+        renderGLEventActuator = RenderGLStateSystem()
+
+
+        eManager._subscribers['OnUpdateWireframe'] = gWindow
+        eManager._actuators['OnUpdateWireframe'] = renderGLEventActuator
+        eManager._subscribers['OnUpdateCamera'] = gWindow 
+        eManager._actuators['OnUpdateCamera'] = renderGLEventActuator
+
+        gWindow._myCamera = view
+
+        while running:
+            running = self.scene.render(running)
+            self.scene.world.traverse_visit(self.renderUpdate, self.scene.world.root)
+            view =  gWindow._myCamera
+            gizmos.update_mouse_position()
+            gizmos.update_view(view)
+            gizmos.get_keyboard_Event()
+            gizmos.update_gizmos()
+            gizmos.update_imgui()
+            model_cube = trans4.trs
+            model_cube2 = trans4_2.trs
+
+            mvp_cube = projMat @ view @ model_cube
+            mvp_cube2 = projMat @ view @ model_cube2
+
+            shaderDec4.setUniformVariable(key='modelViewProj', value=mvp_cube, mat4=True)
+            shaderDec4_2.setUniformVariable(key='modelViewProj', value=mvp_cube2, mat4=True)
+
+            self.scene.render_post()
+            
+        self.scene.shutdown()
