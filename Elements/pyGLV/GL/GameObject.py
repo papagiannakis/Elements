35b27dc6459bfd9769a575b09aa87327ec0445ca
paulzik <paul@oramavr.com>
2023-05-26 21:27:13 +0200

    GameObject improvements

--- Elements/pyGLV/GL/GameObject.py
+++ Elements/pyGLV/GL/GameObject.py
@@ -6,6 +6,9 @@ from Elements.pyGLV.GL.Shader import Shader, ShaderGLDecorator
 from Elements.pyGLV.GL.VertexArray import VertexArray
 from Elements.pyGLV.utils.normals import generateSmoothNormalsMesh
 from Elements.pyGLV.utils.obj_to_mesh import obj_to_mesh
+from Elements.pyGLV.utils.objimporter.entities import ModelEntity
+from Elements.pyGLV.utils.objimporter.model import Model
+from Elements.pyGLV.utils.objimporter.wavefront import Wavefront
 
 class GameObject:
   
@@ -19,38 +22,14 @@ class GameObject:
     def Spawn(_scene, _objectPath, _objectName,  _parent, trs=None, texture=None):
         if trs is None:
             trs = identity()
-        #Create entity to the scene 
-        entityNode = _scene.world.createEntity(Entity(name = _objectName))
-        _scene.world.addEntityChild(_parent, entityNode)
-        _scene.world.addComponent(entityNode, BasicTransform(name = _objectName + "_TRS", trs = trs))
-        gameObjectMesh = _scene.world.addComponent(entityNode, RenderMesh(name = _objectName + "_Mesh"))
-        if texture is None:
-            obj_color = [168 / 255, 168 / 255, 210 / 255, 1.0]
-            vert, ind, col = obj_to_mesh(_objectPath, color=obj_color)
-            vertices, indices, colors, normals = generateSmoothNormalsMesh(vert, ind, col)
 
-            gameObjectMesh.vertex_attributes.append(vertices)
-            gameObjectMesh.vertex_attributes.append(colors)
-            gameObjectMesh.vertex_attributes.append(normals)
-            gameObjectMesh.vertex_index.append(indices)
-            _scene.world.addComponent(entityNode, VertexArray())
-            shaderDec = _scene.world.addComponent(entityNode, ShaderGLDecorator(
-                Shader(vertex_source=Shader.VERT_PHONG_MVP, fragment_source=Shader.FRAG_PHONG)))
-        #else:
-        #    vertices, indices, myuvs, normals = TextureMapping.UVMapping(_objectPath,
-        #                                                                 normals=True)
-        #    gameObjectMesh.vertex_attributes.append(vertices)
-        #    gameObjectMesh.vertex_attributes.append(myuvs)
-        #    gameObjectMesh.vertex_attributes.append(normals)
+        imported_obj:Model = Wavefront(_objectPath, calculate_smooth_normals=False)
 
+        model_entity:ModelEntity = _scene.world.createEntity(ModelEntity(imported_obj))
+        _scene.world.addEntityChild(_parent, model_entity)
+        model_entity.create_entities_and_components(_scene)
 
-        #    gameObjectMesh.vertex_index.append(indices)
-        #    vArray = _scene.world.addComponent(entityNode, VertexArray())
-        #    shaderDec = _scene.world.addComponent(entityNode, ShaderGLDecorator(
-        #        Shader(vertex_source=Shader.SIMPLE_TEXTURE_PHONG_VERT,
-        #               fragment_source=Shader.SIMPLE_TEXTURE_PHONG_FRAG)))
-
-        return shaderDec
+        return model_entity
 
 
     def Find(searchName: str) -> Entity:
