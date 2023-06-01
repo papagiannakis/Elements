2d4db0f6d239f388f9884a3a276dbd5215c9253e
Manos Kamarianakis <m.kamarianakis@gmail.com>
2023-06-01 18:12:43 +0300

    Clean up code

--- Elements/pyECSS/tests/test_GAutils.py
+++ Elements/pyECSS/tests/test_GAutils.py
@@ -6,7 +6,6 @@ from Elements.pyECSS.GA.quaternion import Quaternion
 import unittest
 import numpy as np
 
-# create unittest for GAutils.py
 class TestGAutils(unittest.TestCase):
     
     def setUp(self):
@@ -59,7 +58,6 @@ class TestGAutils(unittest.TestCase):
 
     def test_matrix_to_motor_CGA(self):
         actual_outpout = GAutil.matrix_to_motor(self.M, method = 'CGA').value
-        # print("actual_outpout", actual_outpout)
         expected_output = np.array([ 0.96592583, 0,0,0,0,0, -0.20751688, 0.13834459, -0.48296291, -0.48296291, -0.0691723,-0.96592584,-0.96592584,-1.44888875,-1.44888875,0,0,0,0,0,0,0,0,0,0,0,0.48420606, 0.48420606,0,0,0,0])
         np.testing.assert_array_almost_equal(actual_outpout, expected_output, decimal = 6)
      
@@ -71,13 +69,11 @@ class TestGAutils(unittest.TestCase):
     def test_PGA_vec_to_matrix(self):
         self.PGA_vec = GAutil.matrix_to_motor(self.M, method = 'PGA').value
         self.M_back = GAutil.PGA_vec_to_TRS_matrix(self.PGA_vec)
-        # print("M_back", self.M_back)
         np.testing.assert_array_almost_equal(self.M_back, self.M, decimal = 6)
 
     def test_CGA_vec_to_matrix(self):
         self.CGA_vec = GAutil.matrix_to_motor(self.M, method = 'CGA').value
         self.M_back = GAutil.CGA_vec_to_TRS_matrix(self.CGA_vec)
-        # print("M_back", self.M_back)
         np.testing.assert_array_almost_equal(self.M_back, self.M, decimal = 6)
 
     def test_various_PGA(self):
@@ -103,64 +99,3 @@ class TestGAutils(unittest.TestCase):
         np.testing.assert_array_almost_equal(t, t1, decimal = 6)
         np.testing.assert_array_almost_equal(q, q1, decimal = 6)
         
-
-    # print("Traditional Representation forms:")
-#     extracted_theta, extracted_axis, extracted_translation_vector = matrix_to_angle_axis_translation(M) # notice theta is in rad
-#     q = Quaternion.from_angle_axis(angle = extracted_theta, axis = extracted_axis)
-#     print("extracted theta: ", extracted_theta, "\t VS original :", np.radians(theta))
-#     print("extracted axis: ", extracted_axis, "\t VS original :", axis/np.linalg.norm(axis))
-#     print("extracted translation_vector", extracted_translation_vector, "\t VS original :", translation_vector)
-#     print("extracted quaternion: ", q)
-    #     print("Traditional Representation forms:")
-#     extracted_theta, extracted_axis, extracted_translation_vector = matrix_to_angle_axis_translation(M) # notice theta is in rad
-#     q = Quaternion.from_angle_axis(angle = extracted_theta, axis = extracted_axis)
-#     print("extracted theta: ", extracted_theta, "\t VS original :", np.radians(theta))
-#     print("extracted axis: ", extracted_axis, "\t VS original :", axis/np.linalg.norm(axis))
-#     print("extracted translation_vector", extracted_translation_vector, "\t VS original :", translation_vector)
-#     print("extracted quaternion: ", q)
-    
-    # create test for t_q_to_TR
-    # def test_t_q_to_TR(self):
-    #     # create test cases
-    #     t = [1,2,3]
-    #     q = [4,5,6,7]
-    #     # create expected output
-    #     expected_output = 1
-    #     # create actual output
-    #     actual_output = t_q_to_TR(t,q)
-    #     # compare actual output to expected output
-    #     self.assertEqual(actual_output, expected_output)
-
-
-# if __name__ == '__main__':
-
- 
-    
-
-#     # PGA TESTS
-#     print("="*20)
-#     print("PGA TESTS")
-#     print("="*20)
-#     T = 1 - 0.5*pga.e0*(6*pga.e1 + 7*pga.e2 + 8*pga.e3)
-#     R = 1 +2*pga.e12 + 3*pga.e13 +4*pga.e23
-#     TR = T*R
-
-#     extractedT, extractedR =  extract_T_R_from_TR_PGA(TR)
-#     print("T: ", T)
-#     print("R: ", R)
-#     print("extractedT ", extractedT)
-#     print("extractedR ", extractedR)
-
-#     # CGA TESTS
-#     print("="*20)
-#     print("CGA TESTS")
-#     print("="*20)
-#     t = [1,2,3]
-#     q = [1,2,3,4] # x,y,z,w
-#     TR = t_q_to_TR(t,q)
-
-#     print (TR.value)
-#     t1,q1 = extract_t_q_from_TR(TR, algebra = 'CGA')
-#     print("t = ", t1)
-#     print("q = ", q1)
-
