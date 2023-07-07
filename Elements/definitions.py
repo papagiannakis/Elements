1e46c6237859758ee769fae408a36a5cdb03e3fb
Manos Kamarianakis <m.kamarianakis@gmail.com>
2023-07-07 16:44:03 +0300

    Move files directly under Elements

--- Elements/definitions.py
+++ Elements/definitions.py
@@ -2,7 +2,7 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-MODEL_DIR = os.path.join(ROOT_DIR, "examples", "files", "models")
-TEXTURE_DIR = os.path.join(ROOT_DIR, "examples", "files", "textures")
-SCENES_DIR = os.path.join(ROOT_DIR, "examples", "files", "scenes")
-SCV_DIR = os.path.join(ROOT_DIR, "examples", "files", "scv")
+MODEL_DIR = os.path.join(ROOT_DIR, "files", "models")
+TEXTURE_DIR = os.path.join(ROOT_DIR, "files", "textures")
+SCENES_DIR = os.path.join(ROOT_DIR, "files", "scenes")
+SCV_DIR = os.path.join(ROOT_DIR, "files", "scv")
