81700aba9456ac26d98911a415fea2fe2130c190
Manos Kamarianakis <m.kamarianakis@gmail.com>
2023-07-12 16:02:59 +0300

    Added secondary version of USDImporter for ENGAGE examples

--- Elements/examples/Advanced/GenerativeExampleEngage.py
+++ Elements/examples/Advanced/GenerativeExampleEngage.py
@@ -1,6 +1,6 @@
 import os
 
-print(os.getcwd())
+# print(os.getcwd())
 import sys
 
 # sys.path.append("../../../")
@@ -15,7 +15,7 @@ from Elements.pyGLV.GL.Scene import Scene
 import Elements.utils.Converter as Converter
 import random
 import numpy as np
-import Elements.features.usd.UsdImporter as SceneLoader
+import Elements.features.usd.UsdImporterENGAGE as SceneLoader
 from Elements.pyECSS.Entity import Entity
 
 
