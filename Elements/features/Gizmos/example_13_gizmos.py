e30c435b5bc40cb0efcc4831055766f140f0594c
Chibi Nemesis <paulzotas@hotmail.com>
2023-07-21 19:21:44 +0300

    Replace cubes' shaders with phong shaders

--- Elements/features/Gizmos/example_13_gizmos.py
+++ Elements/features/Gizmos/example_13_gizmos.py
@@ -11,8 +11,20 @@ from Elements.features.Gizmos.Gizmos import Gizmos
 from Elements.pyGLV.GL.Shader import InitGLShaderSystem, Shader, ShaderGLDecorator, RenderGLShaderSystem
 from Elements.pyGLV.GL.VertexArray import VertexArray
 from Elements.utils.terrain import generateTerrain
+import Elements.utils.normals as norm
 
 from OpenGL.GL import GL_LINES
+
+#Light
+Lposition = util.vec(0.0, 2.5, 1.2) #uniform lightpos
+Lambientcolor = util.vec(1.0, 1.0, 1.0) #uniform ambient color
+Lambientstr = 0.3 #uniform ambientStr
+LviewPos = util.vec(2.5, 2.8, 5.0) #uniform viewpos
+Lcolor = util.vec(1.0,1.0,1.0)
+Lintensity = 0.9
+#Material
+Mshininess = 0.4 
+Mcolor = util.vec(0.8, 0.0, 0.8)
 
 scene = Scene()    
 
@@ -94,37 +106,45 @@ renderUpdate = scene.world.createSystem(RenderGLShaderSystem())
 initUpdate = scene.world.createSystem(InitGLShaderSystem())
 
 
+vertices_pink, indices_pink, color_pink, normals_pink = norm.generateFlatNormalsMesh(vertexCube,indexCube,colorCube)
+vertices_yellow, indices_yellow, color_yellow, normals_yellow = norm.generateFlatNormalsMesh(vertexCube,indexCube,colorCube2)
+
 ## ADD CUBE ##
 # attach a simple cube in a RenderMesh so that VertexArray can pick it up
-mesh4_pink_1.vertex_attributes.append(vertexCube)
-mesh4_pink_1.vertex_attributes.append(colorCube)
-mesh4_pink_1.vertex_index.append(indexCube)
+mesh4_pink_1.vertex_attributes.append(vertices_pink)
+mesh4_pink_1.vertex_attributes.append(color_pink)
+mesh4_pink_1.vertex_attributes.append(normals_pink)
+mesh4_pink_1.vertex_index.append(indices_pink)
 vArray4_pink_1 = scene.world.addComponent(node4_pink_1, VertexArray())
-shaderDec4_pink_1 = scene.world.addComponent(node4_pink_1, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+shaderDec4_pink_1 = scene.world.addComponent(node4_pink_1, ShaderGLDecorator(Shader(vertex_source = Shader.VERT_PHONG_MVP, fragment_source=Shader.FRAG_PHONG)))
 
-mesh4_pink_2.vertex_attributes.append(vertexCube)
-mesh4_pink_2.vertex_attributes.append(colorCube)
-mesh4_pink_2.vertex_index.append(indexCube)
+mesh4_pink_2.vertex_attributes.append(vertices_pink)
+mesh4_pink_2.vertex_attributes.append(color_pink)
+mesh4_pink_2.vertex_attributes.append(normals_pink)
+mesh4_pink_2.vertex_index.append(indices_pink)
 vArray4_pink_2 = scene.world.addComponent(node4_pink_2, VertexArray())
-shaderDec4_pink_2 = scene.world.addComponent(node4_pink_2, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+shaderDec4_pink_2 = scene.world.addComponent(node4_pink_2, ShaderGLDecorator(Shader(vertex_source = Shader.VERT_PHONG_MVP, fragment_source=Shader.FRAG_PHONG)))
 
-mesh4_pink_3.vertex_attributes.append(vertexCube)
-mesh4_pink_3.vertex_attributes.append(colorCube)
-mesh4_pink_3.vertex_index.append(indexCube)
+mesh4_pink_3.vertex_attributes.append(vertices_pink)
+mesh4_pink_3.vertex_attributes.append(color_pink)
+mesh4_pink_3.vertex_attributes.append(normals_pink)
+mesh4_pink_3.vertex_index.append(indices_pink)
 vArray4_pink_3 = scene.world.addComponent(node4_pink_3, VertexArray())
-shaderDec4_pink_3 = scene.world.addComponent(node4_pink_3, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+shaderDec4_pink_3 = scene.world.addComponent(node4_pink_3, ShaderGLDecorator(Shader(vertex_source = Shader.VERT_PHONG_MVP, fragment_source=Shader.FRAG_PHONG)))
 
-mesh4_yellow_1.vertex_attributes.append(vertexCube)
-mesh4_yellow_1.vertex_attributes.append(colorCube2)
-mesh4_yellow_1.vertex_index.append(indexCube)
+mesh4_yellow_1.vertex_attributes.append(vertices_yellow)
+mesh4_yellow_1.vertex_attributes.append(color_yellow)
+mesh4_yellow_1.vertex_attributes.append(normals_yellow)
+mesh4_yellow_1.vertex_index.append(indices_yellow)
 vArray4_yellow_1 = scene.world.addComponent(node4_yellow_1, VertexArray())
-shaderDec4_yellow_1 = scene.world.addComponent(node4_yellow_1, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+shaderDec4_yellow_1 = scene.world.addComponent(node4_yellow_1, ShaderGLDecorator(Shader(vertex_source = Shader.VERT_PHONG_MVP, fragment_source=Shader.FRAG_PHONG)))
 
-mesh4_yellow_2.vertex_attributes.append(vertexCube)
-mesh4_yellow_2.vertex_attributes.append(colorCube2)
-mesh4_yellow_2.vertex_index.append(indexCube)
+mesh4_yellow_2.vertex_attributes.append(vertices_yellow)
+mesh4_yellow_2.vertex_attributes.append(color_yellow)
+mesh4_yellow_2.vertex_attributes.append(normals_yellow)
+mesh4_yellow_2.vertex_index.append(indices_yellow)
 vArray4_yellow_2 = scene.world.addComponent(node4_yellow_2, VertexArray())
-shaderDec4_yellow_2 = scene.world.addComponent(node4_yellow_2, ShaderGLDecorator(Shader(vertex_source = Shader.COLOR_VERT_MVP, fragment_source=Shader.COLOR_FRAG)))
+shaderDec4_yellow_2 = scene.world.addComponent(node4_yellow_2, ShaderGLDecorator(Shader(vertex_source = Shader.VERT_PHONG_MVP, fragment_source=Shader.FRAG_PHONG)))
 
 
 # Generate terrain
@@ -183,6 +203,52 @@ eManager._actuators['OnUpdateCamera'] = renderGLEventActuator
 
 gWindow._myCamera = view # otherwise, an imgui slider must be moved to properly update
 
+#Set Uniform Variables for Light and material
+shaderDec4_pink_1.setUniformVariable(key='ambientColor',value=Lambientcolor,float3=True)
+shaderDec4_pink_1.setUniformVariable(key='ambientStr',value=Lambientstr,float1=True)
+shaderDec4_pink_1.setUniformVariable(key='viewPos',value=LviewPos,float3=True)
+shaderDec4_pink_1.setUniformVariable(key='lightPos',value=Lposition,float3=True)
+shaderDec4_pink_1.setUniformVariable(key='lightColor',value=Lcolor,float3=True)
+shaderDec4_pink_1.setUniformVariable(key='lightIntensity',value=Lintensity,float1=True)
+shaderDec4_pink_1.setUniformVariable(key='shininess',value=Mshininess,float1=True)
+shaderDec4_pink_1.setUniformVariable(key='matColor',value=Mcolor,float3=True)
+##
+shaderDec4_pink_2.setUniformVariable(key='ambientColor',value=Lambientcolor,float3=True)
+shaderDec4_pink_2.setUniformVariable(key='ambientStr',value=Lambientstr,float1=True)
+shaderDec4_pink_2.setUniformVariable(key='viewPos',value=LviewPos,float3=True)
+shaderDec4_pink_2.setUniformVariable(key='lightPos',value=Lposition,float3=True)
+shaderDec4_pink_2.setUniformVariable(key='lightColor',value=Lcolor,float3=True)
+shaderDec4_pink_2.setUniformVariable(key='lightIntensity',value=Lintensity,float1=True)
+shaderDec4_pink_2.setUniformVariable(key='shininess',value=Mshininess,float1=True)
+shaderDec4_pink_2.setUniformVariable(key='matColor',value=Mcolor,float3=True)
+##
+shaderDec4_pink_3.setUniformVariable(key='ambientColor',value=Lambientcolor,float3=True)
+shaderDec4_pink_3.setUniformVariable(key='ambientStr',value=Lambientstr,float1=True)
+shaderDec4_pink_3.setUniformVariable(key='viewPos',value=LviewPos,float3=True)
+shaderDec4_pink_3.setUniformVariable(key='lightPos',value=Lposition,float3=True)
+shaderDec4_pink_3.setUniformVariable(key='lightColor',value=Lcolor,float3=True)
+shaderDec4_pink_3.setUniformVariable(key='lightIntensity',value=Lintensity,float1=True)
+shaderDec4_pink_3.setUniformVariable(key='shininess',value=Mshininess,float1=True)
+shaderDec4_pink_3.setUniformVariable(key='matColor',value=Mcolor,float3=True)
+##
+shaderDec4_yellow_1.setUniformVariable(key='ambientColor',value=Lambientcolor,float3=True)
+shaderDec4_yellow_1.setUniformVariable(key='ambientStr',value=Lambientstr,float1=True)
+shaderDec4_yellow_1.setUniformVariable(key='viewPos',value=LviewPos,float3=True)
+shaderDec4_yellow_1.setUniformVariable(key='lightPos',value=Lposition,float3=True)
+shaderDec4_yellow_1.setUniformVariable(key='lightColor',value=Lcolor,float3=True)
+shaderDec4_yellow_1.setUniformVariable(key='lightIntensity',value=Lintensity,float1=True)
+shaderDec4_yellow_1.setUniformVariable(key='shininess',value=Mshininess,float1=True)
+shaderDec4_yellow_1.setUniformVariable(key='matColor',value=Mcolor,float3=True)
+##
+shaderDec4_yellow_2.setUniformVariable(key='ambientColor',value=Lambientcolor,float3=True)
+shaderDec4_yellow_2.setUniformVariable(key='ambientStr',value=Lambientstr,float1=True)
+shaderDec4_yellow_2.setUniformVariable(key='viewPos',value=LviewPos,float3=True)
+shaderDec4_yellow_2.setUniformVariable(key='lightPos',value=Lposition,float3=True)
+shaderDec4_yellow_2.setUniformVariable(key='lightColor',value=Lcolor,float3=True)
+shaderDec4_yellow_2.setUniformVariable(key='lightIntensity',value=Lintensity,float1=True)
+shaderDec4_yellow_2.setUniformVariable(key='shininess',value=Mshininess,float1=True)
+shaderDec4_yellow_2.setUniformVariable(key='matColor',value=Mcolor,float3=True)
+
 while running:
     running = scene.render()
     scene.world.traverse_visit(renderUpdate, scene.world.root)
@@ -216,6 +282,11 @@ while running:
     shaderDec4_pink_3.setUniformVariable(key='modelViewProj', value=mvp_cube_pink_3, mat4=True)
     shaderDec4_yellow_1.setUniformVariable(key='modelViewProj', value=mvp_cube_yellow_1, mat4=True)
     shaderDec4_yellow_2.setUniformVariable(key='modelViewProj', value=mvp_cube_yellow_2, mat4=True)
+    shaderDec4_pink_1.setUniformVariable(key='model', value=model_cube_pink_1, mat4=True)
+    shaderDec4_pink_2.setUniformVariable(key='model', value=model_cube_pink_2, mat4=True)
+    shaderDec4_pink_3.setUniformVariable(key='model', value=model_cube_pink_3, mat4=True)
+    shaderDec4_yellow_1.setUniformVariable(key='model', value=model_cube_yellow_1, mat4=True)
+    shaderDec4_yellow_2.setUniformVariable(key='model', value=model_cube_yellow_2, mat4=True)
     scene.render_post()
     
 scene.shutdown()
