ba2117e8e2fce41ee4b13ad89c69a6e361f7f960
Manos Kamarianakis <m.kamarianakis@gmail.com>
2023-11-13 22:14:18 +0200

    Remove redundant import

--- Elements/features/ObjectPicker/objectPicker_example.py
+++ Elements/features/ObjectPicker/objectPicker_example.py
@@ -13,7 +13,7 @@ from Elements.pyGLV.GL.VertexArray import VertexArray
 import Elements.utils.normals as norm
 from Elements.utils.obj_to_mesh import obj_to_mesh
 from Elements.pyGLV.GL.Textures import get_texture_faces, Texture
-from Elements.pyGLV.GL.Textures import get_single_texture_faces
+
 from Elements.utils.helper_function import displayGUI_text
 from Elements.features.ObjectPicker.AABoundingBox import AABoundingBox
 from OpenGL.GL import GL_LINES
