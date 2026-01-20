24b8ca3a8d114873757985245c47f3e68ef075b0
Paraskevi Mourelatou <paraskevimour@gmail.com>
2026-01-21 01:23:17 +0200

    Add environment mapping implementation with one of the two examples.

---
+++ Elements/extensions/environment_mapping/__init__.py
@@ -0,0 +1,11 @@
+"""
+Environment Mapping Extension
+
+This extension provides environment mapping with reflection effects,
+allowing objects to appear as if they are made of mirror or other
+reflective materials.
+"""
+
+from .environment_mapping import EnvironmentMapping
+
+__all__ = ['EnvironmentMapping']
