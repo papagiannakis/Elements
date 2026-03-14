8d8db020b5e898b53be61e81bae97911a15ca58d
ElisavetKourkounaki <elisabethkourkounaki@gmail.com>
2026-03-14 19:48:54 +0200

    initial files
    I created the hierarchy for the files and added the four initial files with some lines of code that present the pipeline.

---
+++ Elements/extensions/textToScene/src/ir_schema.py
@@ -0,0 +1,15 @@
+scene = {
+    "window": {
+        "width": 1200,
+        "height": 800
+    },
+    "objects": [
+        {
+            "type": "cube",
+            "name": "cube1",
+            "position": [0, 0, 0],
+            "scale": [1, 1, 1],
+            "color": [0.9, 0, 0.9]
+        }
+    ]
+}
